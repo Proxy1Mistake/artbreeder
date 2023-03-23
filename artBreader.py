54de379304e391211fb0e09a6dfa6bd7e8e5702a
Proxy Mallet <qq84063@gmail.com>
2023-03-23 16:27:54 +0500

    adding a new feature

--- artBreader.py
+++ artBreader.py
@@ -7,13 +7,19 @@ class artBreader:
         self.session = Session()
 
     def register(self, email: str, password: str, username: str):
-        data = {f'email': email, 'email_pref': 'false', 'password': password, 'refferal': 'null', 'username': username}
+        data = {
+            'email': email,
+            'email_pref': 'false',
+            'password': password,
+            'refferal': 'null',
+            'username': username
+        }
         req = self.session.post(url = self.url('create_user'), headers = self.headers, json = data)
         return req.text
 
     def login(self, email: str, password: str):
         data = {
-            f'email': email,
+            'email': email,
             'password': password
         }
         req = self.session.post(url = self.url('login'), headers = self.headers, json = data)
@@ -45,6 +51,15 @@ class artBreader:
     def getСreatorData(self, creatorName: str):
         req = self.session.get(url = self.url(f'{creatorName}/__data.json'), headers = self.headers)
         return getСreatorData(req.json()).getСreatorData
+
+    def getImageChildren(self, key: str, limit: int):
+        data = {
+            "image_key": key,
+            "offset": 0,
+            "limit": limit
+        }
+        req = self.session.post(url = self.url('image_children'), headers = self.headers, json = data)
+        return getImageChildren(req.json()).getImageChildren
 
     def getImage(self, key: str):
         req = self.session.get(url = f'https://artbreeder.b-cdn.net/imgs/{key}_small.jpeg', headers = self.headers)
--- util/objects.py
+++ util/objects.py
@@ -313,3 +313,55 @@ class getСreatorData:
             except(KeyError, TypeError): pass
         return self
 
+class getImageChildren:
+    def __init__(self, data):
+        self.json = data
+        self.key = []
+        self.likes = []
+        self.private = []
+        self.prompt = []
+        self.nsfw = []
+        self.creator = []
+        self.imageCreatedAt = []
+        self.creatorName = []
+        self.liked = []
+        self.downloaded = []
+        self.tagged = []
+        self.hasThumbnail = []
+        self.timeString = []
+        self.modelName = []
+        self.size = []
+
+    @property
+    def getImageChildren(self):
+        for x in self.json:
+            try: self.key.append(x['key'])
+            except(KeyError, TypeError): pass
+            try: self.likes.append(x['likes'])
+            except(KeyError, TypeError): pass
+            try: self.private.append(x['private'])
+            except(KeyError, TypeError): pass
+            try: self.prompt.append(x['prompt'])
+            except(KeyError, TypeError): pass
+            try: self.creator.append(x['creator'])
+            except(KeyError, TypeError): pass
+            try: self.imageCreatedAt.append(x['image_created_at'])
+            except(KeyError, TypeError): pass
+            try: self.creatorName.append(x['creator_name'])
+            except(KeyError, TypeError): pass
+            try: self.liked.append(x['liked'])
+            except(KeyError, TypeError): pass
+            try: self.downloaded.append(x['downloaded'])
+            except(KeyError, TypeError): pass
+            try: self.tagged.append(x['tagged'])
+            except(KeyError, TypeError): pass
+            try: self.hasThumbnail.append(x['has_thumbnail'])
+            except(KeyError, TypeError): pass
+            try: self.timeString.append(x['has_thumbnail'])
+            except(KeyError, TypeError): pass
+            try: self.modelName.append(x['model_name'])
+            except(KeyError, TypeError): pass
+            try: self.size.append(x['size'])
+            except(KeyError, TypeError): pass
+        return self
+
